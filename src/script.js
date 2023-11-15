d57f248ed55d4afd590c6abd9e2d9e53f56d99ab
jillianjoy1 <jillianjoy810@gmail.com>
2023-11-14 19:17:02 -0800

    Added weather project code

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
